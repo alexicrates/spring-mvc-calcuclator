92177e885873b8ab1dbbd8b4215df814c5630b7d
alexicrates <maleichuck.sasha@gmail.com>
2022-12-08 17:23:22 +0300

    edited AddingService

--- addition-service/src/main/java/com/example/additionservice/service/AddingService.java
+++ addition-service/src/main/java/com/example/additionservice/service/AddingService.java
@@ -2,20 +2,13 @@ package com.example.additionservice.service;
 
 import com.example.operation.aspects.OperationLog;
 import com.example.operation.domain.Operation;
-import com.example.operation.repository.OperationsRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AddingService {
-    @Autowired
-    OperationsRepository operationsRepository;
-
     @OperationLog
     public Operation calculate(Double firstParameter, Double secondParameter) {
-        Operation operation = new Operation(Operation.Type.SUM, firstParameter, secondParameter,
+        return new Operation(Operation.Type.SUM, firstParameter, secondParameter,
                 firstParameter + secondParameter);
-        operationsRepository.save(operation);
-        return operation;
     }
 }
